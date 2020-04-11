0700110533e4c8c6bf1325572c0826e30e84bd20
Discord-YuulSims <Discord.YuulSims@noreply.github.com>
2020-04-11 15:47:32 +0200

    help richembed
    test fase

--- commands/help.js
+++ commands/help.js
@@ -3,19 +3,25 @@ const botConfig = require("../botconfig.json");
 
 module.exports.run = async (bot, message, args) => {
 
-    /*   try{
-   
-           var text = "**YuulSims Bot** \n\n **__Wat kan ik?__** \n **!?hallo** Als je de bot hallo wilt zeggen. \n **!?botinfo** Als je in info over de bot wil weten. \n **!?serverinfo** Als je info van de server wil weten. \n **!?kick** Niks voor jullie! \n **!?ban** Niks voor jullie! \n **!?help** Is als je dit berichtje wil krijgen. \n **!?prefix** Als de admins de prefix moeten aanpassen. \n **!?clear** Om berichten te verwijderen. \n **!?ping** Het is gewoon iets grappige \n **!?mute** Als iemand de regels heeft overtreden. \n **!?warns** Iemand een waarschuwing geven.(alleen admins kunnen dit) \n **!?ac** Als je een mooi berichtje wilt maken. \n **!?rps** Het is blad steen schaar spel. \n **!?report** Als je vindt dat iemand de regels overtreden heeft. \n **!?tban** Is voor de admins. \n **!?ticket** Als je een vraag, bericht of wat er beter kan in de server. **!?close** Als je je ticket klaar is. \n **!?welkom** Is ook niks voor jullie.";
+     try{
+        var icon = message.guild.iconURL;
+           var text = new discord.RichEmbed()
+           .setTitle("**YuulSims Sever Bot Commands.**")
+           .setThumbnail(icon)
+           .setFooter("©YuulSims Server")
+           .setColor("#00ffb7")
+           .setDescription("**__Algemeen__** \n **!?ac** Maak een mooie een mooi berichtje.")
+           //"**YuulSims Bot** \n\n **__Wat kan ik?__** \n\n **__ALgemeen__** \n **!?ac** Maak een announcement aan.";
    
            message.author.send(text);
    
-           message.reply("Je hebt een privé berichtje gekregen.");
+           message.reply("Al de commando's staan in je privé berichten! :mailbox_with_mail:");
    
        }catch (cerror){
-           message.channel.send("Sorry! Er ging iets mis.");
+           message.channel.send("Je privé berichten staan uit geschakeld, je hebt geen hulp ontvangen.");
    
        }
-   */
+   
 
     /* var commandList = [];
  
@@ -36,7 +42,7 @@ module.exports.run = async (bot, message, args) => {
     var repsonse = "";
  
  */
-var commandsList = [];
+    /*var commandsList = [];
 
     bot.commands.forEach(command => {
 
@@ -71,7 +77,7 @@ var commandsList = [];
 
         message.channel.send("Je privé berichten staan uit geschakeld, je hebt geen hulp ontvangen");
 
-    });
+   }); */ 
 }
 
 module.exports.help = {
