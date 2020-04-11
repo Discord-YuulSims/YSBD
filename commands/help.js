51eb106373fd24dfa6a3b8b00a7ff0ef65c69d98
Discord-YuulSims <Discord.YuulSims@noreply.github.com>
2020-04-11 16:15:42 +0200

    help discordEmbed
    geen gewonen maar zelf verzonen.

--- commands/help.js
+++ commands/help.js
@@ -3,25 +3,25 @@ const botConfig = require("../botconfig.json");
 
 module.exports.run = async (bot, message, args) => {
 
-     try{
+    try {
         var icon = message.guild.iconURL;
-           var text = new discord.RichEmbed()
-           .setTitle("**YuulSims Sever Bot Commands.**")
-           .setThumbnail(icon)
-           .setFooter("©YuulSims Server")
-           .setColor("#00ffb7")
-           .setDescription("**__Algemeen__** \n **!?ac** Maak een mooie een mooi berichtje.")
-           //"**YuulSims Bot** \n\n **__Wat kan ik?__** \n\n **__ALgemeen__** \n **!?ac** Maak een announcement aan.";
-   
-           message.author.send(text);
-   
-           message.reply("Al de commando's staan in je privé berichten! :mailbox_with_mail:");
-   
-       }catch (cerror){
-           message.channel.send("Je privé berichten staan uit geschakeld, je hebt geen hulp ontvangen.");
-   
-       }
-   
+        var text = new discord.RichEmbed()
+            .setTitle("**YuulSims Sever Bot Commando's.**")
+            .setThumbnail(icon)
+            .setFooter("©YuulSims Server")
+            .setColor("#00ffb7")
+            .setDescription("**__Algemeen__** \n **!?hallo** Zeg hallo tegen de bot. \n **!?ping** Laat de responstijd zien. \n **!?idee** Laat een idee achter voor de server. **!?level** Laat zien welke level je bent. \n **!?report** Als je vindt dat iemand de regels overtreedt. \n **review** Laat een review van iemand of iets achter. \n\n **__Infomatie__** \n **!?botinfo** Laat info over de bot zien. \n **!?help** Laat dit bericht zien. \n **!?serverinfo** Laat info over de server zien. \n\n **__Minigames__** \n **!?rps** Speel blad steen schaar. \n **later meer** \n\n **__Ticket__** \n **!?ticket** Maak een ticket aan. \n **!?close** Sluit het ticket. \n\n **__Alleen Admns__** \n **!?ac** Maak een mooi berichtje. \n **!?ban** Om iemand van de server te verbannen. \n **clear** Om berichtjes te verwijderen. \n **!?giveaway** Voor een winactie. \n **!?kick** Om iemand van de server te gooien. \n **!?prefixs** Als de bot zijn prefixs varanderd moeten worden. \n **!?tban** Als iemand tijdelijk verbannen moet worden. \n **!?mute** Als iemand gemuted moet worden. \n **!?warn** Als we iemand moeten waarschuwen. \n **!?welkom** Voor het welkoms bericht.")
+        //"**YuulSims Bot** \n\n **__Wat kan ik?__** \n\n **__ALgemeen__** \n **!?ac** Maak een announcement aan.";
+
+        message.author.send(text);
+
+        message.reply("Al de commando's staan in je privé berichten! :mailbox_with_mail:");
+
+    } catch (cerror) {
+        message.channel.send("Je privé berichten staan uit geschakeld, je hebt geen hulp ontvangen.");
+
+    }
+
 
     /* var commandList = [];
  
@@ -77,7 +77,7 @@ module.exports.run = async (bot, message, args) => {
 
         message.channel.send("Je privé berichten staan uit geschakeld, je hebt geen hulp ontvangen");
 
-   }); */ 
+   }); */
 }
 
 module.exports.help = {
