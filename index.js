2dc1813d8f8e5251c696f28d959f2fd9727f24ca
Discord-YuulSims <Discord.YuulSims@noreply.github.com>
2020-04-24 18:47:41 +0200

    kanalen.js
    een nieuw commando.

--- index.js
+++ index.js
@@ -57,6 +57,7 @@ bot.on("guildMemberAdd", member => {
         .setAuthor(`${member.user.tag}`, member.user.displayAvatarURL)
         .setThumbnail(userIcon)
         .setDescription(`**Hey ${member.user.username},** \n**Welkom** in de server van **YuulSims**.`)
+        .addField("We zijn met", message.guild.memberCount)
         .setColor("#00ff73")
         .setTimestamp()
         .setFooter("©YuulSims Server | Gebruiker gejoind.");
@@ -87,6 +88,7 @@ bot.on("guildMemberRemove", member => {
         .setThumbnail(userIcon)
         .setDescription(`**${member.user.username} heeft de server verlaten.** \n Met pijn in het hart moeten wij afscheidt nemen van ${member.user.username}`)
         .setColor("#ff5500")
+        .addField("We zijn nog met", message.guild.memberCount)
         .setTimestamp()
         .setFooter("©YuulSims Server | Gebruiker geleavd");
 
